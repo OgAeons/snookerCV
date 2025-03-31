11ef2425d3cf4d461e8c73cd6fd80f92f2e31cbb
OgAeons <opaeons@gmail.com>
2025-03-31 12:10:34 +0530

    Added background subtraction to detect the balls

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import cv2 as cv
 import numpy as np
 
+# initialize MOG2 background subtractor
+bg_subtractor = cv.createBackgroundSubtractorMOG2(detectShadows=True)
+
 def track_balls(video):
     cap = cv.VideoCapture(video)
 
@@ -10,13 +13,21 @@ def track_balls(video):
             print("Can't receive frame (stream end?). Exiting ...")
             break
 
+        # convert to grayscale
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-        cv.imshow('Snooker Ball Tracking', gray)
+        # apply gaussian blur to reduce noise
+        blur = cv.GaussianBlur(gray, (5, 5), 0)
+
+        # apply background subtractor
+        fg_mask = bg_subtractor.apply(blur)
+
+        cv.imshow('Snooker Ball Tracking', fg_mask)
+
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv.destroyAllWindows()
 
-track_balls('videos/sample1.mp4')
+track_balls('videos/sample4.mp4')
