2800f1cff4ef1cd81296147450956b2396e7f468
OgAeons <opaeons@gmail.com>
2025-04-02 01:26:35 +0530

    Updated the code to draw boxes and label the balls

--- app.py
+++ app.py
@@ -1,33 +1,47 @@
 import cv2
 from ultralytics import YOLO
 
-# Loaded the custom trained model
+# Load the custom trained model
 model = YOLO('model/snookerCV.pt')
 
 def detect_balls(video):
     cap = cv2.VideoCapture(video)
+    frame_count = 0
 
     while cap.isOpened():
         success, frame = cap.read()
-
         if success:
-            # Run YOLOv11 tracking on the frame, persist tracks between frames
-            results = model.track(frame, persist=True, show=True, tracker="bytetrack.yaml", conf=0.4) 
+            # results from yolov11
+            results = model(frame)
 
-            # Visualizing the results on the frame
-            annotated_frame = results[0].plot()
+            # Convert results to to_df data
+            detections = results[0].to_df()  
 
-            # Displaying the annotated frame
-            cv2.imshow("YOLOv8 Tracking", annotated_frame)
+            for index, row in detections.iterrows():
+                # bounding box coordinates 
+                box = row['box']
+                x1, y1, x2, y2 = int(box['x1']), int(box['y1']), int(box['x2']), int(box['y2'])
 
-            # stop the model 
-            if cv2.waitKey(1) & 0xFF == ord("q"):
+                # class index ie.0
+                ball_id = int(row['class'])  
+
+                # Draw bounding box around detected ball
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+                # Label the ball with its ID
+                cv2.putText(frame, f'Snooker Ball {ball_id}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
+
+            # display the bounding boxes and labels on the video
+            cv2.imshow("Snooker Ball Detection", frame)
+
+            # stop the video
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
-            # end of video
             break
 
     cap.release()
     cv2.destroyAllWindows()
 
+# function call and path to video sample
 detect_balls("videos/sample4.mp4")
