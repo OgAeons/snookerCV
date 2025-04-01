917863cdacf7f31001a8f4e5cccda833920a1174
OgAeons <opaeons@gmail.com>
2025-04-02 01:03:49 +0530

    Added code and tested the custom model

--- app.py
+++ app.py
@@ -1,38 +1,33 @@
 import cv2
 from ultralytics import YOLO
 
-# load YOLOv11 model
-model = YOLO('yolo11n.pt')
+# Loaded the custom trained model
+model = YOLO('model/snookerCV.pt')
 
 def detect_balls(video):
     cap = cv2.VideoCapture(video)
 
     while cap.isOpened():
         success, frame = cap.read()
-        if not success:
-            break
 
-        # run YOLO model on the frame
-        results = model(frame)
+        if success:
+            # Run YOLOv11 tracking on the frame, persist tracks between frames
+            results = model.track(frame, persist=True, show=True, tracker="bytetrack.yaml", conf=0.4) 
 
-        for box in results[0].boxes:
-            x1, y1, x2, y2 = map(int, box.xyxy[0])  # box coordinates
-            conf = box.conf.item()  # confidence score
-            cls = int(box.cls.item())  # class index
+            # Visualizing the results on the frame
+            annotated_frame = results[0].plot()
 
-            # filter detections by confidence threshold
-            if conf > 0.5:
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(frame, f"Ball {conf:.2f}", (x1, y1 - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            # Displaying the annotated frame
+            cv2.imshow("YOLOv8 Tracking", annotated_frame)
 
-        # display the frame
-        cv2.imshow("Snooker Ball Tracking", frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            # stop the model 
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
+            # end of video
             break
 
     cap.release()
     cv2.destroyAllWindows()
 
-detect_balls("videos/sample1.mp4")
+detect_balls("videos/sample4.mp4")
